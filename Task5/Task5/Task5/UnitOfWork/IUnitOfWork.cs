63b1c82cb8218149880899a66b16178f1302a398
Lukyana <31761509+Kiraluka@users.noreply.github.com>
2018-12-12 00:36:53 +0200

    Add files via upload

---
+++ Task5/Task5/Task5/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5.DataTypes;
+using Task5.Repository;
+
+namespace Task3.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        GenericRepository<TaxiClient> Clients { get; }
+        GenericRepository<TaxiDriver> Drivers { get; }
+        GenericRepository<TaxiOrder> Orders { get; }
+    }
+}
