e0563e24abfb87ed23ec56e6a8b0b843f20da589
Valentina <valya.romanchuk3211@gmail.com>
2018-12-12 11:59:08 +0200

    Fixed DataContext

---
+++ Task5/Task5/Task5/DataTypes/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.DataTypes
+{
+    public class DriverContext : DbContext
+    {
+        public DbSet<TaxiDriver> Drivers { get; set; }
+        public DbSet<TaxiClient> Clients { get; set; }
+        public DbSet<TaxiOrder> Orders { get; set; }
+
+        public DriverContext() : base("name=DriverContext")
+        {
+        }
+    }
+}
